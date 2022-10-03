d2df6a62214c6d201760333d2816f5cd5446eecf
Marc Gonzalez <marcgonzaleztarrio@gmail.com>
2022-10-03 09:20:19 +0200

    finish

---
+++ vue-project/src/components/susususu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hola me llamo: {{ name2 }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let name2 = ref("Marc");
+</script>
+
+<style></style>
